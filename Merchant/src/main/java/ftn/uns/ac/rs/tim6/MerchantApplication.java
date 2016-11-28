d63b2b06e001492bd41d548b2f8b51edb1f85c42
MIKO\Milos <milosvujosevic@hotmail.com>
2016-11-28 22:45:57 +0100

    test komit

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MerchantApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MerchantApplication.class, args);
+		SpringApplication.run(MerchantApplication.class, args); // kako je
 	}
 }
