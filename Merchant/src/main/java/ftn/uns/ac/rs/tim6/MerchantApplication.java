5112ca8dc07eaaa41b7406d46278aabf24a23bc9
MIKO\Milos <milosvujosevic@hotmail.com>
2016-12-01 00:01:54 +0100

    model na pola

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
@@ -2,12 +2,7 @@ package ftn.uns.ac.rs.tim6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import ftn.uns.ac.rs.tim6.model.Age;
-
-import ftn.uns.ac.rs.tim6.model.Age;
 
 @SpringBootApplication
 //@RestController
