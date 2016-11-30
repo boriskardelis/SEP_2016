ae1cdca6a267bdac904d0a1e10acc1fbf0aa3778
MIKO\Milos <milosvujosevic@hotmail.com>
2016-12-01 00:19:23 +0100

    aaaaaaaaaaaaa

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
@@ -2,22 +2,23 @@ package ftn.uns.ac.rs.tim6;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import ftn.uns.ac.rs.tim6.model.Age;
+
 
 @SpringBootApplication
+//@RestController
 public class MerchantApplication {
 
+   /* @RequestMapping("/")
+    public String home() {
+        return "Hello Docker World asda ";
+    }*/
+	
+
     public static void main(String[] args) {
-    	System.out.println("ASDADA");
-    	
-    	Age newAge = new Age();
-    	long id = 12312312;
-    	newAge.setId(id);
-    	newAge.setAgeRiskValue(1.3);
-    	 
+    	System.out.println("ASDADA");	 
         SpringApplication.run(MerchantApplication.class, args);
         
-        System.out.println(newAge.getId());
+       
     }
 
 }
