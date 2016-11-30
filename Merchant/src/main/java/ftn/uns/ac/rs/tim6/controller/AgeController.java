338b21deffb58ae33a982651c9420cc8bfd4539d
Makso-PC\Makso <makso.the.one@gmail.com>
2016-11-30 16:55:53 +0100

    Odradjena bekend inicijalizacija

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/AgeController.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/AgeController.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ftn.uns.ac.rs.tim6.controller;
 
 import java.util.List;
@@ -35,26 +35,4 @@ public class AgeController {
 	}
 	
 	}
-=======
-package ftn.uns.ac.rs.tim6.controller;
 
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import ftn.uns.ac.rs.tim6.model.Age;
-
-@RestController
-@RequestMapping("/api")
-public class AgeController {
-
-	@RequestMapping(value = "/age",						//test 
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Age> getAllAges() {
-        return null;
-    }
-	
-
-}
->>>>>>> branch 'master' of https://github.com/boriskardelis/SEP_2016
--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/service/GenericService.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/service/GenericService.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package ftn.uns.ac.rs.tim6.service;
 
 import java.util.List;
@@ -9,15 +9,4 @@ public interface GenericService<T> {
     T save(T t);
 
 }
-=======
-package ftn.uns.ac.rs.tim6.service;
 
-import java.util.List;
-
-public interface GenericService<T> {
-	
-	List<T> getAll();
-    T save(T t);
-
-}
->>>>>>> branch 'master' of https://github.com/boriskardelis/SEP_2016
