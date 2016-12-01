df5483d1bf32f8923af2909f3ba629ea6cab5b91
DESKTOP-Q9MIR62\Boris K <boriskardelis@gmail.com>
2016-12-01 21:05:52 +0100

    GET u kontrolerima

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/PackagesController.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/PackagesController.java
@@ -1,9 +1,15 @@
 package ftn.uns.ac.rs.tim6.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import ftn.uns.ac.rs.tim6.model.Packages;
 import ftn.uns.ac.rs.tim6.service.PackagesService;
 
 @RestController
@@ -12,4 +18,10 @@ public class PackagesController {
 
 	@Autowired
 	PackagesService packagesService;
+	
+	@RequestMapping(value = "/packages", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public List<Packages> handleGetAllPackage() {
+		return packagesService.getAll();
+	}
 }
