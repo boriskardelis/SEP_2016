6bd1aec106ecbf60dc790127c549ce04889d4c3e
Makso-PC\Makso <makso.the.one@gmail.com>
2016-12-01 22:13:10 +0100

    a

---
+++ Merchant/src/main/webapp/app/components/home/homeCtrl.js
@@ -0,0 +1,19 @@
+(function() {
+	'use strict';
+
+	angular
+		.module('merchantApp')
+		.controller('HomeCtrl', HomeCtrl);
+
+	HomeCtrl.$inject = ['$scope', 'HomeService' ];
+	function HomeCtrl($scope, HomeService) {
+		var vm = this;
+
+		vm.proba = "RADI";
+		vm.hello = HomeService.sayHello();
+		vm.test = "aaaa";
+	
+
+
+	}
+})();
