d07af3d43b4c5319e172be0be3b006fabbce121a
Makso-PC\Makso <makso.the.one@gmail.com>
2016-12-01 22:11:29 +0100

    Prvi zahtev prema bekendu preko http-a

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/AgeController.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/AgeController.java
@@ -1,7 +1,10 @@
 
 package ftn.uns.ac.rs.tim6.controller;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
@@ -13,18 +16,22 @@ import ftn.uns.ac.rs.tim6.service.AgeService;
 @RestController
 @RequestMapping("/api")
 public class AgeController {
-	
+
 	@Autowired
 	AgeService ageService;
 
-	
-	@RequestMapping(value = "/ages",						 
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/ages", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	@ResponseBody
-	public List<Age> accountSummary() {
-	    return ageService.getAll();
+	public List<Age> handleGetAllAges() {
+		return ageService.getAll();
 	}
 	
+/*	@RequestMapping(value = "/ages", method = RequestMethod.POST)
+	public Age handlePostAge(@Valid @RequestBody Age ageResource) {
+		Age age = new Age();
+		age.setAge(ageResource.getAge());
+		Age saved = ageService.save(age);
+		return new Age(saved.getAge(), saved.getRiskValue());
 	}
-
+*/
+}
--- Merchant/src/main/webapp/app/components/buy/buyService.js
+++ Merchant/src/main/webapp/app/components/buy/buyService.js
@@ -9,13 +9,29 @@
 	function BuyService($http) {
 
 		var getRegions = function() {
-			return ["Monaco", "Serbia"];
+			console.log("USAO U METODU GETregions");
+			return $http.get("api/regions")
+						.then(function(response) {
+							console.log('RESPONSE OD SERVISA');
+							return response;
+						});
+			
+		};
+
+		var getSumTo = function() {
+			return ["10.000 EUR", "50.0000 EUR"];
+		};
+
+		var getAgeCarrire = function() {
+			return ["1-2", "17-36"];
 		};
 
 
 		return {
 			
-			getRegions : getRegions
+			getRegions : getRegions,
+			getSumTo : getSumTo,
+			getAgeCarrire : getAgeCarrire
 			
 		};
 
