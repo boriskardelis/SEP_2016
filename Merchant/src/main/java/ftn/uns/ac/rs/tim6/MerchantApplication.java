53abe435fc1ad3a6dbb665ca8a8d58cc62363225
DESKTOP-Q9MIR62\Boris K <boriskardelis@gmail.com>
2016-12-07 19:27:18 +0100

    drools

--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/MerchantApplication.java
@@ -3,6 +3,11 @@ package ftn.uns.ac.rs.tim6;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.drools.runtime.StatefulKnowledgeSession;
+
+import ftn.uns.ac.rs.tim6.model.Buyer;
+import ftn.uns.ac.rs.tim6.util.DroolsReadKnowlageBase;
+
 @SpringBootApplication
 public class MerchantApplication {
 
@@ -10,7 +15,26 @@ public class MerchantApplication {
 		
 		SpringApplication.run(MerchantApplication.class, args);
 		System.out.println("Pokrenuta aplikacija");
+		
+		
+		
+		try {
+			DroolsReadKnowlageBase kbase = new DroolsReadKnowlageBase();
+			StatefulKnowledgeSession ksession = kbase.getSession();
+			Buyer buyer = new Buyer(10);
+			ksession.insert(buyer);
+			
+			System.out.println("buyerov telefon " + buyer.getPhoneNumber());
+			
+			ksession.fireAllRules();
+			
+			System.out.println("buyerov telefon " + buyer.getPhoneNumber());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+        
+	
 
 	}
-
 }
--- Merchant/src/main/java/ftn/uns/ac/rs/tim6/model/Buyer.java
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/model/Buyer.java
@@ -16,7 +16,7 @@ public class Buyer implements Serializable{
 
 	@Id
 	@GeneratedValue
-	@Column(name = "ID")
+	@Column(name = "BUYER_ID")
 	private Long id;
 	
 	@Column(name = "LAST_NAME")
@@ -41,6 +41,122 @@ public class Buyer implements Serializable{
 	private int phoneNumber;
 	
 	
+
+	public Long getId() {
+		return id;
+	}
+
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+
+	public int getJMBG() {
+		return JMBG;
+	}
+
+
+
+	public void setJMBG(int jMBG) {
+		JMBG = jMBG;
+	}
+
+
+
+	public int getPassportNumber() {
+		return passportNumber;
+	}
+
+
+
+	public void setPassportNumber(int passportNumber) {
+		this.passportNumber = passportNumber;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+
+	public String getAddress() {
+		return address;
+	}
+
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+
+	public int getPhoneNumber() {
+		return phoneNumber;
+	}
+
+
+
+	public void setPhoneNumber(int phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+    
+
+	public Buyer(Long id, String lastName, int jMBG, int passportNumber, String email, String firstName, String address,
+			int phoneNumber) {
+		super();
+		this.id = id;
+		this.lastName = lastName;
+		JMBG = jMBG;
+		this.passportNumber = passportNumber;
+		this.email = email;
+		this.firstName = firstName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+	
+	//test
+	public Buyer(int phoneNumber){
+		this.phoneNumber = phoneNumber;
+	}
+
+
 
 	public Buyer() {
 		super();
