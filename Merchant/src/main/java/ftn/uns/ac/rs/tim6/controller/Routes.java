1bb2a3484293fc88999890aab1bd749a60443527
MIKO\Milos <milosvujosevic@hotmail.com>
2016-12-14 20:34:42 +0100

    kontroler da refresh page radi

---
+++ Merchant/src/main/java/ftn/uns/ac/rs/tim6/controller/Routes.java
@@ -0,0 +1,25 @@
+package ftn.uns.ac.rs.tim6.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public  class Routes {
+
+    @RequestMapping({
+        "/about",
+        "/buy",
+        "/home",
+        "/tracks/{id:\\w+}",
+        "/buy/firstStep",
+        "/buy/secondStep",
+        "/buy/thirdStep",
+        "/buy/fourthStep",
+        "/buy/fifthStep",
+        
+       
+    })
+    public String index() {
+        return "forward:/index.html";
+    }
+}
