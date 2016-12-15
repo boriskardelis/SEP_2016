b58c8095a28c6dad0e7479352e0703c3ac815d6d
DESKTOP-Q9MIR62\Boris K <boriskardelis@gmail.com>
2016-12-15 22:57:46 +0100

    PCC first commit

---
+++ PCC/src/main/java/ftn/uns/ac/rs/tim6/PccApplication.java
@@ -0,0 +1,12 @@
+package ftn.uns.ac.rs.tim6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PccApplication.class, args);
+	}
+}
